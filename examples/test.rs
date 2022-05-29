ff622f40b89dd024f43b47c1bacec5ef879628b2
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-05-29 21:49:32 +0200

    Work on example

--- examples/test.rs
+++ examples/test.rs
@@ -1,17 +1,21 @@
 use fush::{and, fush};
 
 #[fush]
-fn my_fun(x: fush::F32, y: fush::F32) -> fush::F32 {
+fn foo(x: fush::F32, y: fush::F32) -> fush::F32 {
     let z = fush::var(x * y);
     let w = fush::var(y + x + 1.0);
 
     fush::branch(and(z.eq(w), z.eq(1.0)), z * 2.0, 1.0)
 }
 
+#[fush]
+fn bar(x: fush::F32) -> fush::F32 {
+    foo(x, x * 2.0) + 5.0
+}
+
 fn main() {
     let a = 2.0;
-    let b = 3.0;
 
-    let result = my_fun(a, b);
+    let result = bar(a);
     println!("{:#?}", result);
 }
