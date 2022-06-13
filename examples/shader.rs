b76c2d78584e5f15f8a8c9cbd9844164f8913ba4
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-06-14 00:53:17 +0200

    Brainstorming shader types again

--- examples/shader.rs
+++ examples/shader.rs
@@ -1,70 +1,62 @@
-use posh::{var, vec3, FSIn, FSOut, IntoPosh, Posh, Shader, VSIn, VSOut};
+use posh::{posh, var, vec3, FSIn, FSOut, IntoPosh, Posh, Shader, VSIn, VSOut};
 
 #[derive(IntoPosh)]
+#[posh(UniformBlock)]
 struct ModelToClip {
     model_to_view: [f32; 3],
     view_to_clip: [f32; 3],
 }
 
-#[derive(IntoPosh)]
-struct ParamSet {
-    modelview: [f32; 3],
-}
-
-impl posh::Resource for _PoshParamSet {
-    fn func_arg() -> Self {
-        todo!()
-    }
+#[derive(Resources)]
+struct Params {
+    one: ModelToClip,
+    two: ModelToClip,
 }
 
 #[derive(IntoPosh)]
+#[posh(Vertex)]
 struct Vertex {
     position: [f32; 3],
     normal: [f32; 3],
     thickness: f32,
 }
 
-impl posh::Vertex for _PoshVertex {}
-
 #[derive(IntoPosh)]
+#[posh(Vertex)]
 struct Instance {
     color: [f32; 3],
 }
 
-impl posh::Vertex for _PoshInstance {}
-
 #[derive(IntoPosh)]
-struct Varying {
+#[posh(VertexOut)]
+struct VertexOut {
     color: [f32; 3],
     normal: [f32; 3],
 }
 
-impl posh::VertexOut for _PoshVarying {}
-
 #[derive(IntoPosh)]
-struct Fragment {
+#[posh(FragmentOut)]
+struct FragmentOut {
     color: [f32; 3],
     normal: [f32; 3],
 }
 
-impl posh::FragmentOut for _PoshFragment {}
-
-fn vertex(params: Posh<ParamSet>, input: VSIn<Vertex>) -> VSOut<Varying> {
+fn vertex(params: Posh<ParamSet>, input: VSIn<Vertex>) -> VSOut<VertexOut> {
     VSOut {
-        position: params.modelview * input.vertex.position,
-        varying: Posh::<Varying> {
+        position: params.one.view_to_clip * params.one.model_to_view * input.vertex.position,
+        varying: Posh::<VertexOut> {
             color: vec3(255.0, 0.0, 0.0),
-            normal: params.modelview * input.vertex.normal,
+            normal: params.two.model_to_view * input.vertex.normal,
         },
     }
 }
 
 fn vertex2(params: Posh<ParamSet>, input: VSIn<(Vertex, Instance)>) -> VSOut<Varying> {
     VSOut {
-        position: params.modelview * input.vertex.0.position,
+        position: params.one.model_to_view * input.vertex.0.position,
         varying: Posh::<Varying> {
             color: input.vertex.1.color,
-            normal: params.modelview * input.vertex.0.normal,
+            normal: params.one.model_to_view * input.vertex.0.normal,
         },
     }
 }
