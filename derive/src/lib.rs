10a92ca0020ed8c351e943659d039c69133e0f48
leod <subtle.frustration@proton.me>
2022-12-16 23:43:40 +0100

    Add sekeleton for Value derive

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,12 +1,13 @@
+mod utils;
+mod value;
+
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
-
-mod utils;
 
 #[proc_macro_derive(Value)]
 pub fn derive_value(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    match expose::derive(input) {
+    match value::derive(input) {
         Ok(ts) => ts,
         Err(e) => e.to_compile_error(),
     }
--- derive/src/utils.rs
+++ derive/src/utils.rs
@@ -1,18 +1,8 @@
-use syn::{parse_quote, Data, Error, Field, Fields, Ident, Path, Result};
+use syn::{Data, Error, Field, Fields, Ident, Result};
 
 pub fn get_struct_fields(ident: &Ident, data: Data) -> Result<Vec<Field>> {
-    let fields = if let Data::Struct(data) = data {
-        match data.fields {
-            Fields::Named(fields) => Ok(fields.named.into_iter().collect()),
-            Fields::Unnamed(_) => Err(Error::new_spanned(
-                ident,
-                "posh derive macros do not support tuple structs",
-            )),
-            Fields::Unit => Err(Error::new_spanned(
-                ident,
-                "posh derive macros do not support unit structs",
-            )),
-        }
+    let data = if let Data::Struct(data) = data {
+        Ok(data)
     } else {
         Err(Error::new_spanned(
             ident,
@@ -20,10 +10,11 @@ pub fn get_struct_fields(ident: &Ident, data: Data) -> Result<Vec<Field>> {
         ))
     }?;
 
-    Ok(fields)
-}
-
-pub trait Derivable {
-    fn path() -> Path;
-    
+    match data.fields {
+        Fields::Named(fields) => Ok(fields.named.into_iter().collect()),
+        Fields::Unnamed(_) | Fields::Unit => Err(Error::new_spanned(
+            ident,
+            "posh derive macros do not support tuple or unit structs",
+        )),
+    }
 }
