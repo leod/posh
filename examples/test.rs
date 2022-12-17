d986c2f262b8d5dc1aaa3b178dbed6c7896708dd
Leonard Dahlmann <leo.dahlmann@gmail.com>
2022-12-17 16:33:01 +0100

    Add Uniform to example

--- examples/test.rs
+++ examples/test.rs
@@ -1,11 +1,43 @@
 use posh::{
-    sl::{self, Value},
-    Numeric,
+    sl::{self, Object, Value},
+    Gl, Numeric, Sl, UniformDomain,
 };
 
 #[derive(Value)]
 struct Foo<T: Numeric> {
     x: sl::Scalar<T>,
+}
+
+struct MyUniform<D: UniformDomain, T: Numeric> {
+    x: D::Vec2<T>,
+}
+
+impl<T: Numeric> Object for MyUniform<Sl, T> {
+    const TY: posh::dag::Ty = todo!();
+
+    fn expr(&self) -> std::rc::Rc<posh::dag::Expr> {
+        todo!()
+    }
+}
+
+impl<T: Numeric> Value for MyUniform<Sl, T> {
+    fn from_expr(expr: posh::dag::Expr) -> Self {
+        todo!()
+    }
+}
+
+impl<T: Numeric> Object for MyUniform<Gl, T> {
+    const TY: posh::dag::Ty = todo!();
+
+    fn expr(&self) -> std::rc::Rc<posh::dag::Expr> {
+        todo!()
+    }
+}
+
+impl<T: Numeric> Value for MyUniform<Gl, T> {
+    fn from_expr(expr: posh::dag::Expr) -> Self {
+        todo!()
+    }
 }
 
 fn main() {}
