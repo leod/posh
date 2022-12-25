1c32e09a45cec5558e6ad1272c21f34133540dcb
leod <subtle.frustration@proton.me>
2022-12-25 20:34:09 +0100

    Neatly split off ToValue derive
    This makes it possible to derive both Vertex and Uniform for the same struct. If anyone ever wants to do that.

---
+++ derive/src/to_value.rs
@@ -0,0 +1,120 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{parse_quote, DeriveInput, Ident, Result};
+
+use crate::utils::{remove_domain_param, SpecializeDomain, StructFields};
+
+pub fn derive(input: DeriveInput) -> Result<TokenStream> {
+    let ident = &input.ident;
+    let ident_str = ident.to_string();
+
+    let sl_field_types_trait = Ident::new(
+        &format!("PoshInternal{ident}ToValueSlFieldTypes"),
+        ident.span(),
+    );
+
+    let generics_no_d = remove_domain_param(ident, &input.generics)?;
+
+    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
+    let (impl_generics_no_d, _, where_clause_no_d) = generics_no_d.split_for_impl();
+
+    let ty_generics_sl = SpecializeDomain::new(parse_quote!(::posh::Sl), ident, &input.generics)?;
+
+    let fields = StructFields::new(&input.ident, &input.data)?;
+    let field_idents = fields.idents();
+    let field_types = fields.types();
+    let field_strings = fields.strings();
+
+    Ok(quote! {
+        // Helper trait for mapping struct field types to `Sl`.
+        trait #sl_field_types_trait {
+            #(
+                #[allow(non_camel_case_types)]
+                type #field_idents: ::posh::sl::Value;
+            )*
+        }
+
+        // Implement the helper trait for mapping struct field types to `Sl`.
+        impl #impl_generics #sl_field_types_trait for #ident #ty_generics
+        #where_clause
+        {
+            #(
+                type #field_idents = <#field_types as ::posh::sl::ToValue>::Output;
+            )*
+        }
+
+        // Implement `Struct` for the struct in `Sl`.
+        impl #impl_generics_no_d ::posh::sl::Struct for #ident #ty_generics_sl
+        #where_clause
+        {
+            const STRUCT_TY: ::posh::dag::StructTy = ::posh::dag::StructTy {
+                name: #ident_str,
+                fields: &[
+                    #(
+                        (
+                            #field_strings,
+                            <
+                                <#ident #ty_generics_sl as #sl_field_types_trait>::#field_idents
+                                as ::posh::sl::Object
+                            >::TY,
+                        )
+                    ),*
+                ],
+                is_built_in: false,
+            };
+        }
+
+        // Implement `Object` for the struct in `Sl`.
+        impl #impl_generics_no_d ::posh::sl::Object for #ident #ty_generics_sl
+        #where_clause_no_d
+        {
+            const TY: ::posh::dag::Ty = ::posh::dag::Ty::Base(::posh::dag::BaseTy::Struct(
+                &<Self as ::posh::sl::Struct>::STRUCT_TY,
+            ));
+
+            fn expr(&self) -> ::std::rc::Rc<::posh::dag::Expr> {
+                ::posh::sl::primitives::simplify_struct_literal(
+                    &<Self as ::posh::sl::Struct>::STRUCT_TY,
+                    &[
+                        #(
+                            ::posh::sl::Object::expr(&self.#field_idents)
+                        ),*
+                    ]
+                )
+            }
+        }
+
+        // Implement `Value` for the struct in `Sl`.
+        impl #impl_generics_no_d ::posh::sl::Value for #ident #ty_generics_sl
+        #where_clause_no_d
+        {
+            fn from_expr(expr: ::posh::dag::Expr) -> Self {
+                let base = ::std::rc::Rc::new(expr);
+
+                Self {
+                    #(
+                        #field_idents: ::posh::sl::primitives::field(
+                            base.clone(),
+                            #field_strings,
+                        )
+                    ),*
+                }
+            }
+        }
+
+        // Implement `ToValue` for the struct in all domains.
+        impl #impl_generics ::posh::sl::ToValue for #ident #ty_generics
+        #where_clause
+        {
+            type Output = #ident #ty_generics_sl;
+
+            fn to_value(self) -> Self::Output {
+                Self::Output {
+                    #(
+                        #field_idents: ::posh::sl::ToValue::to_value(self.#field_idents)
+                    ),*
+                }
+            }
+        }
+    })
+}
