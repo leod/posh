cf2ffadd494c71ebefe679771d48f3ef3fba1bb5
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-01-01 19:28:26 +0100

    Start with Glow

--- src/gl.rs
+++ src/gl.rs
@@ -6,9 +6,11 @@ mod sampler;
 mod surface;
 mod texture;
 mod uniform_buffer;
-mod vertex_array;
 mod vertex_buffer;
+mod vertex_data;
 mod vertex_stream;
+
+pub mod untyped;
 
 pub use context::Context;
 pub use draw_params::DrawParams;
@@ -18,6 +20,8 @@ pub use sampler::{Sampler2d, Sampler2dBinding};
 pub use surface::{DefaultSurface, SurfaceBinding};
 pub use texture::Texture2dBinding;
 pub use uniform_buffer::{UniformBuffer, UniformBufferBinding};
-pub use vertex_array::VertexArray;
 pub use vertex_buffer::{VertexBuffer, VertexBufferBinding};
+pub use vertex_data::VertexData;
 pub use vertex_stream::{GeometryType, VertexStream};
+
+pub(crate) use context::ContextShared;
--- src/gl/vertex_array.rs
+++
@@ -1,7 +0,0 @@
-use std::marker::PhantomData;
-
-use crate::{Sl, Vertex};
-
-pub struct VertexArray<V: Vertex<Sl>> {
-    _phantom: PhantomData<V>,
-}
