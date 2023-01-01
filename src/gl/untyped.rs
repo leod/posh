f709a62ac840ec46a046fc11faa4b0a4dc9af0ba
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-01-01 23:42:44 +0100

    Prepare for VertexData<V>

--- src/gl/untyped.rs
+++ src/gl/untyped.rs
@@ -2,7 +2,7 @@ pub mod buffer;
 pub mod vertex_data;
 
 pub use buffer::Buffer;
-pub use vertex_data::{VertexData, VertexDataEntry};
+pub use vertex_data::{VertexData, VertexDataEntryInfo};
 
 pub(crate) use buffer::BufferShared;
 pub(crate) use vertex_data::VertexDataAttributeInfo;
--- src/gl/vertex_data.rs
+++ src/gl/vertex_data.rs
@@ -1,7 +1,11 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, rc::Rc};
 
 use crate::{Sl, VertexInterface};
 
 pub struct VertexData<V: VertexInterface<Sl>> {
     _phantom: PhantomData<V>,
+}
+
+impl<V: VertexInterface<Sl>> VertexData<V> {
+    pub fn new(gl: Rc<glow::Context>) {}
 }
