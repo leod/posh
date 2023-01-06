6a5da9dadb8c674d9a0796fec3a6f9594d5f2d68
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-01-06 20:03:47 +0100

    Collect scopes and print deps

--- src/gen/scope_form.rs
+++ src/gen/scope_form.rs
@@ -1,6 +1,4 @@
-use std::{collections::HashMap, rc::Rc};
-
-use crate::{dag::Expr, gen::ExprKey};
+use std::collections::{BTreeMap, BTreeSet};
 
 use super::{SimplifiedExpr, VarForm, VarId};
 
@@ -8,14 +6,14 @@ use super::{SimplifiedExpr, VarForm, VarId};
 pub struct ScopeId(usize);
 
 pub struct Scope {
-    pub simplified_expr: SimplifiedExpr,
+    pub expr: Box<SimplifiedExpr>,
 }
 
 pub struct ScopeForm {}
 
 impl ScopeForm {
     pub fn from_var_form(var_form: VarForm) -> Self {
-        let mut scopes = HashMap::new();
+        let mut scopes = BTreeMap::new();
 
         let mut add_scope = |scope: Scope| {
             let scope_id = ScopeId(scopes.len());
@@ -23,39 +21,58 @@ impl ScopeForm {
             scopes.insert(scope_id, scope);
         };
 
-        /*
+        for (_, var_expr) in var_form.var_exprs() {
+            use SimplifiedExpr::*;
 
-            for var in var_form.vars() {
-                use Expr::*;
-
-                match &var.simplified_expr.expr {
-                    Branch { cond, yes, no, .. } => {
-                        println!("{yes}");
-                        add_scope(Scope {
-                            simplified_expr: var_form.simplified_expr(ExprKey::from(yes)).clone(),
-                        });
-                        add_scope(Scope {
-                            simplified_expr: var_form.simplified_expr(ExprKey::from(no)).clone(),
-                        });
-                    }
-                    Arg { .. }
-                    | ScalarLiteral { .. }
-                    | StructLiteral { .. }
-                    | Binary { .. }
-                    | CallFuncDef { .. }
-                    | CallBuiltIn { .. }
-                    | Field { .. } => (),
+            match var_expr {
+                Branch { yes, no, .. } => {
+                    add_scope(Scope { expr: yes.clone() });
+                    add_scope(Scope { expr: no.clone() });
                 }
+                Arg { .. }
+                | ScalarLiteral { .. }
+                | Binary { .. }
+                | CallFunc { .. }
+                | Field { .. }
+                | Var { .. } => (),
             }
+        }
 
-            for (scope_id, scope) in scopes.iter() {
-                println!(
-                    "{}: {} @ {:?}",
-                    scope_id.0, scope.simplified_expr.expr, scope.simplified_expr.deps
-                );
-            }
-        */
+        for (scope_id, scope) in scopes.iter() {
+            let mut deps = BTreeSet::new();
+            collect_deps(&scope.expr, &mut deps);
+
+            println!("{}: {} @ {:?}", scope_id.0, scope.expr, deps);
+        }
 
         todo!()
+    }
+}
+
+fn collect_deps(expr: &SimplifiedExpr, deps: &mut BTreeSet<VarId>) {
+    use SimplifiedExpr::*;
+
+    match expr {
+        Branch { cond, yes, no, .. } => {
+            collect_deps(cond, deps);
+            collect_deps(yes, deps);
+            collect_deps(no, deps);
+        }
+        Arg { .. } | ScalarLiteral { .. } => (),
+        Binary { left, right, .. } => {
+            collect_deps(left, deps);
+            collect_deps(right, deps);
+        }
+        CallFunc { args, .. } => {
+            for arg in args {
+                collect_deps(arg, deps);
+            }
+        }
+        Field { base, .. } => {
+            collect_deps(base, deps);
+        }
+        Var { id, .. } => {
+            deps.insert(*id);
+        }
     }
 }
