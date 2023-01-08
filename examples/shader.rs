1c377b3fa009dd5574950c1ed802ffa474e36455
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-01-08 18:23:11 +0100

    Simply build up the scope tree in reverse topological order, it is not that hard

--- examples/shader.rs
+++ examples/shader.rs
@@ -18,40 +18,17 @@ struct ColorVertex<D: Domain = Sl> {
 
 fn vertex_shader(globals: Globals, input: VertexInput<ColorVertex>) -> VertexOutput<sl::Vec4<f32>> {
     let shift = globals.offset * globals.time;
-    let shift = globals
+    let mut shift = globals
         .invert
         .branch(shift, false.to_value().branch(shift * -1.0, shift * -2.0));
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    /*let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);
-    let shift = globals.invert.branch(shift, shift * -1.0);*/
+
+    for _ in 0..30 {
+        shift = globals.invert.branch(shift, {
+            let x = shift * 5.0;
+
+            false.to_value().branch(x * -1.0, x * -2.0)
+        });
+    }
 
     let position = input.vertex.position + shift;
 
