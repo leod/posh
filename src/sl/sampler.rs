41d881932c251989150a9e91cf779a0ee8b373f9
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-01-22 15:48:25 +0100

    Sampler/texture confusion [WIP]

--- src/sl/sampler.rs
+++ src/sl/sampler.rs
@@ -1,22 +1,54 @@
 use std::{marker::PhantomData, rc::Rc};
 
+use sealed::sealed;
+
 use crate::{
     dag::{BaseType, Expr, Trace, Type},
     Numeric,
 };
 
-use super::{primitives::built_in_1, Object, Vec2, Vec4};
+use super::{
+    primitives::{built_in_1, cast},
+    Object, Scalar, Value, Vec2, Vec3, Vec4,
+};
+
+/// A value returned by a sampler.
+#[sealed]
+pub trait Sample: Value {
+    /// Type of a single component of a sample.
+    type Component: Numeric;
+}
+
+#[sealed]
+impl<T: Numeric> Sample for Scalar<T> {
+    type Component = T;
+}
+
+#[sealed]
+impl<T: Numeric> Sample for Vec2<T> {
+    type Component = T;
+}
+
+#[sealed]
+impl<T: Numeric> Sample for Vec3<T> {
+    type Component = T;
+}
+
+#[sealed]
+impl<T: Numeric> Sample for Vec4<T> {
+    type Component = T;
+}
 
 /// An object which can be sampled.
 #[derive(Debug, Copy, Clone)]
-pub struct Sampler2d<T: Numeric> {
+pub struct Sampler2d<V> {
     trace: Trace,
-    _phantom: PhantomData<T>,
+    _phantom: PhantomData<V>,
 }
 
-impl<T: Numeric> Object for Sampler2d<T> {
+impl<V: Sample> Object for Sampler2d<V> {
     fn ty() -> Type {
-        Type::Base(BaseType::Sampler2d(T::NUMERIC_TYPE))
+        Type::Base(BaseType::Sampler2d(V::Component::NUMERIC_TYPE))
     }
 
     fn expr(&self) -> Rc<Expr> {
@@ -34,8 +66,10 @@ impl<T: Numeric> Object for Sampler2d<T> {
     }
 }
 
-impl<T: Numeric> Sampler2d<T> {
-    pub fn lookup(self, tex_coords: Vec2<f32>) -> Vec4<T> {
-        built_in_1("texture", tex_coords)
+impl<V: Sample> Sampler2d<V> {
+    pub fn lookup(self, tex_coords: Vec2<f32>) -> V {
+        let sample: Vec4<V::Component> = built_in_1("texture", tex_coords);
+
+        cast(sample)
     }
 }
