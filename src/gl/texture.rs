5d1ad4d5b3e96e5f6935da661f978e8759062c61
leod <subtle.frustration@proton.me>
2023-01-29 17:20:57 +0100

    Add table with format/type/internalformat combinations we want to support

--- src/gl/texture.rs
+++ src/gl/texture.rs
@@ -7,18 +7,9 @@ use crate::sl;
 pub enum Texture2dBinding {}
 
 #[sealed]
-pub trait TextureDim {}
-
-pub struct Dim2d;
-
-pub struct Dim3d;
-
-pub struct Dim2dArray;
-
-pub struct DimCubeMap;
-
-#[sealed]
-pub trait TextureValue {}
+pub trait TextureFormat {
+    //type
+}
 
 pub struct Texture<Dim, Val = sl::Vec4<f32>> {
     _phantom: PhantomData<(Dim, Val)>,
@@ -35,3 +26,45 @@ where
     Val: TextureValue,
 {
 }
+
+/*
+
+RGBA - UNSIGNED_BYTE - RGBA8, SRGB8_ALPHA8
+RGBA - BYTE          - RGBA8_SNORM
+RGBA - FLOAT         - RGBA32F, RGBA16F
+
+RGB  - UNSIGNED_BYTE - RGB8, SRGB8
+RGB  - BYTE          - RGB8_SNORM
+RGB  - FLOAT         - RGB32, RGB16F
+
+RG   - UNSIGNED_BYTE - RG8
+RG   - BYTE          - RG8_SNORM
+RG   - FLOAT         - RG32F, RG16F
+
+RED  - UNSIGNED_BYTE - R8
+RED  - BYTE          - R8_SNORM
+RED  - FLOAT         - R32F, R16F
+
+RGBA_INTEGER - UNSIGNED_BYTE - RGBA8UI
+RGBA_INTEGER - BYTE          - RGBA8I
+...
+
+RGB_INTEGER  - UNSIGNED_BYTE  - RGB8UI
+RGB_INTEGER  - BYTE           - RGB8I
+...
+
+RG_INTEGER   - UNSIGNED_BYTE  - RG8UI
+RG_INTEGER   - BYTE           - RG 8I
+...
+
+RED_INTEGER  - UNSIGNED_BYTE  - R8UI
+RED_INTEGER  - BYTE           - R8I
+...
+
+DEPTH_COMPONENT - UNSIGNED_SHORT - DEPTH_COMPONENT16
+DEPTH_COMPONENT - UNSIGNED_INT   - DEPTH_COMPONENT24
+DEPTH_COMPONENT - FLOAT          - DEPTH_COMPONENT32F
+
+DEPTH_STENCIL   - UNSIGNED_INT_24_8 - DEPTH24_STENCIL8
+
+*/
