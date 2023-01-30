05c4d947f15b95190837aec6b6dc56c099fb0d79
leod <subtle.frustration@proton.me>
2023-01-30 23:10:33 +0100

    WIP

--- src/gl/untyped/texture.rs
+++ src/gl/untyped/texture.rs
@@ -20,6 +20,16 @@ impl Texture2d {
     fn new(gl: Rc<glow::Context>, data: ImageData) -> Result<Self, TextureError> {
         let id = unsafe { gl.create_texture() }.map_err(TextureError::Create)?;
 
+        unsafe {
+            gl.tex_storage_2d(
+                glow::TEXTURE_2D,
+                1,
+                data.internal_format.to_gl(),
+                i32::try_from(data.size[0]).unwrap(),
+                i32::try_from(data.size[1]).unwrap(),
+            );
+        }
+
         Ok(todo!())
     }
 }
