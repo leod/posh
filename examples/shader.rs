5c101519f039cd96fb8072085b68f4ffbd8ee7ab
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-04-26 02:33:23 +0200

    Provide associated consts for Mat*

--- examples/shader.rs
+++ examples/shader.rs
@@ -40,7 +40,7 @@ fn vertex_shader(globals: Globals, vertex: ColorVertex) -> VaryingOutput<sl::Vec
         sl::branch(false, x * -1.0, y.as_vec2() * -2.0)
     });
 
-    let position = sl::Mat2::identity() * vertex.position + shift2 + sl::Mat2::diagonal(4.0).x_axis;
+    let position = sl::Mat2::IDENTITY * vertex.position + shift2 + sl::Mat2::diagonal(4.0).x_axis;
 
     let offsets = sl::array([
         position,
