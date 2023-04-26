09197ff3db891d923e091697d94a82c3c1537fef
leod <subtle.frustration@proton.me>
2023-04-26 02:27:39 +0200

    Provide associated consts for Scalar* and Vec*

--- examples/shader.rs
+++ examples/shader.rs
@@ -42,7 +42,12 @@ fn vertex_shader(globals: Globals, vertex: ColorVertex) -> VaryingOutput<sl::Vec
 
     let position = sl::Mat2::identity() * vertex.position + shift2 + sl::Mat2::diagonal(4.0).x_axis;
 
-    let offsets = sl::array([position, position, sl::vec2(0.3, 0.4), sl::vec2(0.5, 0.6)]);
+    let offsets = sl::array([
+        position,
+        position,
+        sl::vec2(0.3, 0.4) * sl::Vec2::ONE,
+        sl::vec2(0.5, 0.6),
+    ]);
 
     VaryingOutput {
         varying: sl::Vec4::splat(0.0)
