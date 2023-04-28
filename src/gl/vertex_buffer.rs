aa2ee3e7395b7cd4633308a2f10b609b3791d5b4
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-04-28 16:37:44 +0200

    Make re-export conditional on feature

--- src/gl/vertex_buffer.rs
+++ src/gl/vertex_buffer.rs
@@ -49,7 +49,7 @@ where
     }
 
     pub fn set(&self, data: &[B::Gl]) {
-        self.raw.set(&data);
+        self.raw.set(data);
     }
 
     pub fn as_binding(&self) -> VertexBufferBinding<B> {
