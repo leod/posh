1441d87287997b02d8ffcd324c6fa45eb26ba2d5
Leonard Dahlmann <leo.dahlmann@gmail.com>
2023-08-15 21:16:13 +0200

    Allow `sl` tuples up to 8 elements (including: 1 element, which was missing)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 mod interface;
 
+/// Imagine macro parameters, but more like those Russian dolls.
+///
+/// Calls m!(A, B, C), m!(A, B), m!(B), and m!() for i.e. (m, A, B, C)
+/// where m is any macro, for any number of parameters.
+///
+/// Copied from Ralith's hecs crate.
+macro_rules! smaller_tuples_too {
+    ($m: ident, $ty: ident) => {
+        $m!{}
+        $m!{$ty}
+    };
+    ($m: ident, $ty: ident, $($tt: ident),*) => {
+        smaller_tuples_too!{$m, $($tt),*}
+        $m!{$ty, $($tt),*}
+    };
+}
+
 pub mod gl;
 #[macro_use]
 pub mod sl;
